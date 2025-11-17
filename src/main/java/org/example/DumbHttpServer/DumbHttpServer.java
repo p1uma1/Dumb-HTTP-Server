cfa95d1ddc859c3b524653c83b2fefa5111836ed
Nimsara Piumal <nimsarapiumalsac@gmail.com>
2025-11-17 13:17:54 +0530

    Refactor handleClient method to use ByteStreamReader

--- src/main/java/org/example/DumbHttpServer/DumbHttpServer.java
+++ src/main/java/org/example/DumbHttpServer/DumbHttpServer.java
@@ -1,12 +1,14 @@
 package org.example.DumbHttpServer;
 
-
+import org.example.httpServer.utils.ByteStreamReader;
 
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -38,6 +40,84 @@ public class DumbHttpServer {
 
     }
 
+//    public void handleClient(Socket clientSocket) throws IOException {
+//        String body = """
+//                <html>
+//                    <head>
+//                        <title>Home</title>
+//                    </head>
+//                    <body>
+//                        <h1>Home Page</h1>
+//                        <p>Java Tutorials</p>
+//                        <ul>
+//                            <li>
+//                                <a href="/get-started-with-java-series"> Java </a>
+//                            </li>
+//                            <li>
+//                                <a href="/spring-boot"> Spring </a>
+//                            </li>
+//                            <li>
+//                                <a href="/learn-jpa-hibernate"> Hibernate </a>
+//                            </li>
+//                        </ul>
+//                     </body>
+//                 </html>
+//            """;
+//        byte[] bodyBytes = body.getBytes(StandardCharsets.UTF_8);
+//        int length = bodyBytes.length;
+//
+//        InputStream input = clientSocket.getInputStream();
+//
+//
+//        BufferedReader in =
+//                new BufferedReader(new InputStreamReader(input,StandardCharsets.UTF_8));
+//        BufferedWriter out = new BufferedWriter(
+//                new OutputStreamWriter( clientSocket.getOutputStream())
+//        );
+//        String inputLine = "";
+//        StringBuilder requestHeaders = new StringBuilder();
+//
+//        while((inputLine=in.readLine())!=null && !inputLine.isEmpty()){
+//            requestHeaders.append(inputLine).append("\r\n");
+//        }
+//        requestHeaders.append("\r\n");
+//        while((inputLine=in.readLine())!=null && !inputLine.isEmpty()){
+//            requestHeaders.append(inputLine).append("\r\n");
+//        }
+//        System.out.print(requestHeaders);
+//        if(!requestHeaders.isEmpty())
+//        {
+//            RequestHandler requestHandler = new RequestHandler(requestHeaders.toString(), clientSocket);
+//            HashMap<String, String> headers = requestHandler.getHeaders(requestHeaders.toString());
+//            String reqbody = requestHandler.parseRequest().getBody();
+//            String contentLength = headers.get("Content-Length");
+//
+//            System.out.print("body: \n" + reqbody);
+//
+//
+////            int bufferSize = Integer.parseInt(contentLength);
+////            byte[] buffer=new byte[bufferSize];
+////            int totalBytesRead = 0;
+////            int bytesRead=0;
+//
+////            while (totalBytesRead < bufferSize && (bytesRead = input.read(buffer, totalBytesRead, bufferSize - totalBytesRead)) != -1) {
+////                totalBytesRead += bytesRead;
+////                System.out.println("bytes read: "+bytesRead);
+////            }
+////            System.out.println(buffer);
+//
+//            LocalDateTime now = LocalDateTime.now();
+//
+//            out.write("HTTP/1.1 200 OK\r\nDate: " + now + "\r\nServer: Custom Server\r\nContent-Type: text/html\r\nContent-Length: " + length + "\r\n\r\n");
+//            out.write(body);
+//            out.flush();
+//        }
+//
+//
+//
+//
+//    }
+
     public void handleClient(Socket clientSocket) throws IOException {
         String body = """
                 <html>
@@ -64,31 +144,29 @@ public class DumbHttpServer {
         byte[] bodyBytes = body.getBytes(StandardCharsets.UTF_8);
         int length = bodyBytes.length;
 
+        InputStream input = clientSocket.getInputStream();
+        ByteArrayInputStream b;
 
-        BufferedReader in = new BufferedReader(
-                new InputStreamReader(clientSocket.getInputStream())
-        );
+        
         BufferedWriter out = new BufferedWriter(
                 new OutputStreamWriter( clientSocket.getOutputStream())
         );
-        String inputLine;
-        String requestString = "";
-        while((inputLine=in.readLine())!=null && !inputLine.isEmpty()){
-            requestString+=inputLine+'\n';
-            if(inputLine.isEmpty()) break;
+
+        ByteStreamReader in = new ByteStreamReader(input,1024,clientSocket);
+        String headers = in.read();
+        System.out.println(headers);
+
+
+
+
+            LocalDateTime now = LocalDateTime.now();
+
+            out.write("HTTP/1.1 200 OK\r\nDate: " + now + "\r\nServer: Custom Server\r\nContent-Type: text/html\r\nContent-Length: " + length + "\r\n\r\n");
+            out.write(body);
+            out.flush();
         }
 
-        System.out.print("request body: \n"+new RequestHandler(requestString,clientSocket).passBody());
 
-        System.out.print("raw request \n"+requestString);
-        // DumbHttpResponse = new RequestHandler(requestString,clientSocket).getHttpResponse();
-
-        LocalDateTime now = LocalDateTime.now();
-
-        out.write("HTTP/1.1 200 OK\r\nDate: " + now + "\r\nServer: Custom Server\r\nContent-Type: text/html\r\nContent-Length: " + length + "\r\n\r\n");
-        out.write(body);
-        out.flush();
 
 
     }
-}
